1f0ec2cd1acefb438cdbd1fd8ff2e858abcc8fc8
Tofu-Xx <1714843907@qq.com>
2024-11-19 07:16:40 +0800

    chore!: ok

--- vue.js
+++ vue.js
@@ -1,68 +1,44 @@
-var Vue = /** @class */ (function () {
-    function Vue(opt) {
-        this.el = opt.el;
-        this.data = opt.data;
+const { reactive, effect } = VueReactivity;
+window.Vue = class Vue {
+    el;
+    data;
+    methods;
+    constructor(opt) {
+        this.el = opt.el instanceof Element
+            ? opt.el
+            : document.querySelector(opt.el) ?? document.body;
+        this.data = reactive(opt.data);
         this.methods = opt.methods;
-        this._init();
+        this.#init();
     }
-    Vue.prototype._init = function () {
-        var el = document.querySelector(this.el);
-        if (!el)
-            return;
-        this._proxyData(this.data);
-        this._proxyMethods(this.methods);
-        this._render(el);
-    };
-    Vue.prototype._proxyData = function (data) {
-        var keys = Object.keys(data);
-        var _loop_1 = function (key) {
-            Object.defineProperty(this_1, key, {
-                get: function () {
-                    return data[key];
-                },
-                set: function (newValue) {
-                    data[key] = newValue;
-                    this._render();
+    #init() {
+        const walker = document.createTreeWalker(this.el, NodeFilter.SHOW_ELEMENT);
+        while (walker.nextNode()) {
+            const node = walker.currentNode;
+            /* vAttr */
+            [...node.attributes].forEach((attr) => {
+                if (attr.name.startsWith("@")) {
+                    node[attr.name.replace("@", "on")] = this.methods[attr.value].bind(this.data);
+                }
+                if (attr.name.startsWith(":")) {
+                    const attrName = attr.name.slice(1);
+                    effect(() => attrName in node
+                        ? (node[attrName] = this.data[attr.value])
+                        : node.setAttribute(attrName, this.data[attr.value]));
                 }
             });
-        };
-        var this_1 = this;
-        for (var _i = 0, keys_1 = keys; _i < keys_1.length; _i++) {
-            var key = keys_1[_i];
-            _loop_1(key);
+            /* mustache */
+            node.childNodes.forEach((child) => {
+                if (child.nodeType !== Node.TEXT_NODE)
+                    return;
+                const tem = child.textContent ?? "";
+                effect(() => {
+                    child.textContent = [...tem.matchAll(/\{\{(.*?)\}\}/g)].reduce((acc, cur) => {
+                        return acc.replace(cur[0], this.data[cur[1].trim()]);
+                    }, tem);
+                });
+            });
         }
-    };
-    Vue.prototype._proxyMethods = function (methods) {
-        var keys = Object.keys(methods);
-        for (var _i = 0, keys_2 = keys; _i < keys_2.length; _i++) {
-            var key = keys_2[_i];
-            this[key] = methods[key].bind(this);
-        }
-    };
-    Vue.prototype._render = function (el) {
-        el.innerHTML = this._compile(el.innerHTML);
-    };
-    Vue.prototype._compile = function (template) {
-        var _this = this;
-        var reg = /\{\{(.*?)\}\}/g;
-        return template.replace(reg, function (match, key) {
-            console.log(key);
-            return _this._getValue(key.trim());
-        });
-    };
-    Vue.prototype._getValue = function (exp) {
-        return this._getObjValue(this, exp);
-    };
-    Vue.prototype._getObjValue = function (obj, exp) {
-        var keys = exp.split('.');
-        var result = obj;
-        for (var _i = 0, keys_3 = keys; _i < keys_3.length; _i++) {
-            var key = keys_3[_i];
-            if (result) {
-                result = result[key];
-            }
-        }
-        return result;
-    };
-    return Vue;
-}());
+    }
+};
+/*  */
--- vue.ts
+++ vue.ts
@@ -1,24 +1,60 @@
-declare const VueReactiveity: any;
-const {reactive,effect} = VueReactiveity 
+declare const VueReactivity: any;
+const { reactive, effect } = VueReactivity;
 
 type Opt = {
-  el: string;
+  el: string | Element;
   data: Record<string, any>;
   methods: Record<string, Function>;
 };
-class Vue {
-  el: Opt["el"];
+interface Window {
+  Vue: any;
+}
+window.Vue = class Vue {
+  el: Element;
   data: Opt["data"];
   methods: Opt["methods"];
   constructor(opt: Opt) {
-    this.el = opt.el;
-    this.data = opt.data;
+    this.el = opt.el instanceof Element
+      ? opt.el
+      : document.querySelector(opt.el) ?? document.body;
+    this.data = reactive(opt.data);
     this.methods = opt.methods;
-    this._init();
+    this.#init();
   }
-  _init() {
-    const el = typeof this.el === "string" && document.querySelector(this.el);
-    if (!el) return;
+  #init() {
+    const walker = document.createTreeWalker(this.el, NodeFilter.SHOW_ELEMENT);
+    while (walker.nextNode()) {
+      const node = walker.currentNode as Element;
+      /* vAttr */
+      [...node.attributes].forEach((attr) => {
+        if (attr.name.startsWith("@")) {
+          node[attr.name.replace("@", "on")] = this.methods[attr.value].bind(
+            this.data,
+          );
+        }
+        if (attr.name.startsWith(":")) {
+          const attrName = attr.name.slice(1);
+          effect(() =>
+            attrName in node
+              ? (node[attrName] = this.data[attr.value])
+              : node.setAttribute(attrName, this.data[attr.value])
+          );
+        }
+      });
+      /* mustache */
+      node.childNodes.forEach((child) => {
+        if (child.nodeType !== Node.TEXT_NODE) return;
+        const tem = child.textContent ?? "";
+        effect(() => {
+          child.textContent = [...tem.matchAll(/\{\{(.*?)\}\}/g)].reduce(
+            (acc, cur) => {
+              return acc.replace(cur[0], this.data[cur[1].trim()]);
+            },
+            tem,
+          );
+        });
+      });
+    }
   }
-}
+};
 /*  */
