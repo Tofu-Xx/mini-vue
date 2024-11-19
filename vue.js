9536197ac4099804ccef2401c03b44deae0127f7
Tofu-Xx <1714843907@qq.com>
2024-11-19 20:03:26 +0800

    chore: update

--- vue.js
+++ vue.js
@@ -10,21 +10,15 @@ const reactive = (target) => new Proxy(target, [['get', track], ['set', trigger]
     return toDeps(args[1]), res;
 }, acc), {}));
 window.Vue = class Vue {
-    constructor(opt) {
+    constructor (opt) {
         this.el = opt.el instanceof Element ? opt.el : document.querySelector(opt.el) ?? document.createDocumentFragment();
         this.data = reactive(opt.data);
         this.methods = opt.methods;
         this.#entry(this.el, document.createTreeWalker(this.el, NodeFilter.SHOW_ELEMENT));
     }
     #entry(node, walker) {
-        [...node.attributes].forEach((attr) => {
-            attr.name.startsWith("@") && node.addEventListener(attr.name.slice(1), this.methods[attr.value].bind(this.data));
-            attr.name.startsWith(":") && effect(() => node.setAttribute(node.slot = attr.name.slice(1), node[node.slot] = this.data[attr.value]));
-        });
-        node.childNodes.forEach((child) => {
-            const tem = child.nodeType === Node.TEXT_NODE && child.textContent;
-            tem && effect(() => child.textContent = [...tem.matchAll(/\{\{(.*?)\}\}/g)].reduce((acc, cur) => acc.replace(cur[0], this.data[cur[1].trim()]), tem));
-        });
+        [...node.attributes].forEach((attr) => attr.name[0] === "@" ? node.addEventListener(attr.name.slice(1), this.methods[attr.value].bind(this.data)) : attr.name[0] === ":" && effect(() => node.setAttribute(node.slot = attr.name.slice(1), node[node.slot] = this.data[attr.value])));
+        node.childNodes.forEach((child) => (node.slot = child.nodeType === Node.TEXT_NODE && child.textContent) && effect(() => child.textContent = [...node.slot.matchAll(/\{\{(.*?)\}\}/g)].reduce((acc, cur) => acc.replace(cur[0], this.data[cur[1].trim()]), node.slot)));
         walker.nextNode() && this.#entry(walker.currentNode, walker);
     }
 };
--- vue.ts
+++ vue.ts
@@ -31,14 +31,8 @@ window.Vue = class Vue {
     this.#entry(this.el,document.createTreeWalker(this.el, NodeFilter.SHOW_ELEMENT));
   }
   #entry(node:Element,walker:TreeWalker) {
-    [...node.attributes].forEach((attr) => {
-      attr.name.startsWith("@") && node.addEventListener(attr.name.slice(1), this.methods[attr.value].bind(this.data))
-      attr.name.startsWith(":") && effect(() => node.setAttribute(node.slot = attr.name.slice(1), node[node.slot] = this.data[attr.value]));
-    });
-    node.childNodes.forEach((child) => {
-      const tem = child.nodeType === Node.TEXT_NODE && child.textContent
-      tem && effect(() => child.textContent = [...tem.matchAll(/\{\{(.*?)\}\}/g)].reduce((acc, cur) => acc.replace(cur[0], this.data[cur[1].trim()]), tem))
-    });
+    [...node.attributes].forEach((attr) => attr.name[0] === "@" ? node.addEventListener(attr.name.slice(1), this.methods[attr.value].bind(this.data)) : attr.name[0] === ":" && effect(() => node.setAttribute(node.slot = attr.name.slice(1), node[node.slot] = this.data[attr.value])));
+    node.childNodes.forEach((child) => (node.slot = child.nodeType === Node.TEXT_NODE && child.textContent) && effect(() => child.textContent = [...node.slot.matchAll(/\{\{(.*?)\}\}/g)].reduce((acc, cur) => acc.replace(cur[0], this.data[cur[1].trim()]), node.slot)));
     walker.nextNode() && this.#entry(walker.currentNode as Element,walker)
   }
 };
