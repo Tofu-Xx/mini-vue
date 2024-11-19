5fc4cc2d8d9d2e9c9b2b054704cd39f9413b02be
Tofu-Xx <1714843907@qq.com>
2024-11-20 01:54:37 +0800

    feat: watch

--- vue.js
+++ vue.js
@@ -1,10 +1,10 @@
 class Vue {
-  #activeEffect = () => { };
+  activeEffect = () => { };
   #depsMap = Object.create(null);
   constructor (opt) {
     this.el = opt.el instanceof Element ? opt.el : document.querySelector(opt.el) ?? document.createDocumentFragment();
     this.data = new Proxy(opt.data instanceof Function ? opt.data() : opt.data, {
-      get: (...args) => [Reflect.get(...args), (this.#depsMap[args[1]] ||= new Set()).add(this.#activeEffect)][0],
+      get: (...args) => [Reflect.get(...args), (this.#depsMap[args[1]] ||= new Set()).add(this.activeEffect)][0],
       set: (...args) => [Reflect.set(...args), this.#depsMap[args[1]].forEach(f => f())][0],
     });
     this.methods = opt.methods;
@@ -16,6 +16,7 @@ class Vue {
       if (node instanceof Text && (node.__v_tem__ = node.textContent) && (node.__v__ = Array(...node.__v_tem__.matchAll(/\{\{(.*?)\}\}/g))).length)
         effect(() => node.textContent = node.__v__.reduce((acc, cur) => acc.replace(cur[0], this.data[cur[1].trim()]), node.__v_tem__));
       walker.nextNode() && entry.bind(this)(walker.currentNode, walker, effect);
-    }).bind(this)(this.el, document.createTreeWalker(this.el, NodeFilter.SHOW_ALL), fn => (this.#activeEffect = fn, fn()));
+    }).bind(this)(this.el, document.createTreeWalker(this.el, NodeFilter.SHOW_ALL), fn => (this.activeEffect = fn, fn()));
+    Object.keys(opt.watch).forEach(k => this.#depsMap[k].add(() => setTimeout(() => opt.watch[k].bind(this.data)(this.data[k]), 0)));
   }
 }
