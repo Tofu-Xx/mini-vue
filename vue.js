a1850bfe4e0fe1a9efb8fc041826b7f59f7c77c2
Tofu-Xx <1714843907@qq.com>
2024-11-19 16:34:16 +0800

    chore: over

--- vue.js
+++ vue.js
@@ -6,25 +6,21 @@ function effect(fn) {
 const depsMap = Object.create(null);
 const track = (key) => (depsMap[key] ||= new Set()).add(activeEffect);
 const trigger = (key) => depsMap[key].forEach((effect) => effect());
-/* reactive */
-const createHandler = (type) => (...args) => {
-    const res = Reflect[type](...args);
-    type === "get" && track(args[0]);
-    type === "set" && trigger(args[0]);
-    return res;
-};
-const reactive = (target) => new Proxy(target, {
-    get: createHandler("get"),
-    set: createHandler("set"),
-});
+const createHandler = () => Object.fromEntries([['get', track], ['set', trigger]].map(([type, toDeps]) => [
+    type,
+    (...args) => {
+        const res = Reflect[type](...args);
+        toDeps(args[1]);
+        return res;
+    }
+]));
+const reactive = (target) => new Proxy(target, createHandler());
 window.Vue = class Vue {
     el;
     data;
     methods;
     constructor(opt) {
-        this.el = opt.el instanceof Element
-            ? opt.el
-            : document.querySelector(opt.el) ?? document.body;
+        this.el = opt.el instanceof Element ? opt.el : document.querySelector(opt.el) ?? document.body;
         this.data = reactive(opt.data);
         this.methods = opt.methods;
         this.#init();
@@ -33,28 +29,19 @@ window.Vue = class Vue {
         const walker = document.createTreeWalker(this.el, NodeFilter.SHOW_ELEMENT);
         while (walker.nextNode()) {
             const node = walker.currentNode;
-            /* vAttr */
             [...node.attributes].forEach((attr) => {
-                if (attr.name.startsWith("@")) {
+                if (attr.name.startsWith("@"))
                     node[attr.name.replace("@", "on")] = this.methods[attr.value].bind(this.data);
-                }
                 if (attr.name.startsWith(":")) {
                     const attrName = attr.name.slice(1);
-                    effect(() => attrName in node
-                        ? (node[attrName] = this.data[attr.value])
-                        : node.setAttribute(attrName, this.data[attr.value]));
+                    effect(() => attrName in node ? (node[attrName] = this.data[attr.value]) : node.setAttribute(attrName, this.data[attr.value]));
                 }
             });
-            /* mustache */
             node.childNodes.forEach((child) => {
                 if (child.nodeType !== Node.TEXT_NODE)
                     return;
                 const tem = child.textContent ?? "";
-                effect(() => {
-                    child.textContent = [...tem.matchAll(/\{\{(.*?)\}\}/g)].reduce((acc, cur) => {
-                        return acc.replace(cur[0], this.data[cur[1].trim()]);
-                    }, tem);
-                });
+                effect(() => child.textContent = [...tem.matchAll(/\{\{(.*?)\}\}/g)].reduce((acc, cur) => acc.replace(cur[0], this.data[cur[1].trim()]), tem));
             });
         }
     }
--- vue.ts
+++ vue.ts
@@ -1,4 +1,3 @@
-
 /* effect */
 type Key = string | symbol;
 let activeEffect: any = null;
@@ -8,20 +7,27 @@ function effect(fn: Function) {
 }
 const depsMap = Object.create(null);
 const track = (key: Key) => (depsMap[key] ||= new Set()).add(activeEffect);
-const trigger = (key: Key) =>
-  depsMap[key].forEach((effect: Function) => effect());
-
+const trigger = (key: Key) => depsMap[key].forEach((effect: Function) => effect());
 /* reactive */
-const createHandler = (type: "get" | "set") =>  (...args: [any, any,any]) => {
-    const res = Reflect[type](...args);
-    type === "get" && track(args[0]);
-    type === "set" && trigger(args[0]);
-    return res;
-};
-const reactive = <T extends object>(target: T) => new Proxy(target, {
-  get: createHandler("get"),
-  set: createHandler("set"),
-});
+// const createHandler = (type: "get" | "set") =>  (...args: [any, any,any]) => {
+//     const res = Reflect[type](...args);
+//     type === "get" && track(args[0]);
+//     type === "set" && trigger(args[0]);
+//     return res;
+// };
+// const reactive = <T extends object>(target: T) => new Proxy(target, {
+//   get: createHandler("get"),
+//   set: createHandler("set"),
+// });
+type HandlerMap = ["get" | "set", (key: Key) => void][];
+const createHandler = () => Object.fromEntries(([['get',track],['set',trigger]] as HandlerMap).map(([type,toDeps])=>[
+  type,
+  (...args: [any, any, any])=>{
+  const res = Reflect[type](...args);
+  toDeps(args[1]);
+  return res;
+}]));
+const reactive = <T extends object>(target: T) => new Proxy(target, createHandler());
 /* Vue */
 type Opt = {
   el: string | Element;
@@ -36,9 +42,7 @@ window.Vue = class Vue {
   data: Opt["data"];
   methods: Opt["methods"];
   constructor(opt: Opt) {
-    this.el = opt.el instanceof Element
-      ? opt.el
-      : document.querySelector(opt.el) ?? document.body;
+    this.el = opt.el instanceof Element? opt.el: document.querySelector(opt.el) ?? document.body;
     this.data = reactive(opt.data);
     this.methods = opt.methods;
     this.#init();
@@ -47,34 +51,18 @@ window.Vue = class Vue {
     const walker = document.createTreeWalker(this.el, NodeFilter.SHOW_ELEMENT);
     while (walker.nextNode()) {
       const node = walker.currentNode as Element;
-      /* vAttr */
       [...node.attributes].forEach((attr) => {
-        if (attr.name.startsWith("@")) {
-          node[attr.name.replace("@", "on")] = this.methods[attr.value].bind(
-            this.data,
-          );
-        }
+        if (attr.name.startsWith("@")) 
+          node[attr.name.replace("@", "on")] = this.methods[attr.value].bind(this.data);
         if (attr.name.startsWith(":")) {
           const attrName = attr.name.slice(1);
-          effect(() =>
-            attrName in node
-              ? (node[attrName] = this.data[attr.value])
-              : node.setAttribute(attrName, this.data[attr.value])
-          );
+          effect(() => attrName in node? (node[attrName] = this.data[attr.value]): node.setAttribute(attrName, this.data[attr.value]));
         }
       });
-      /* mustache */
       node.childNodes.forEach((child) => {
         if (child.nodeType !== Node.TEXT_NODE) return;
         const tem = child.textContent ?? "";
-        effect(() => {
-          child.textContent = [...tem.matchAll(/\{\{(.*?)\}\}/g)].reduce(
-            (acc, cur) => {
-              return acc.replace(cur[0], this.data[cur[1].trim()]);
-            },
-            tem,
-          );
-        });
+        effect(() => child.textContent = [...tem.matchAll(/\{\{(.*?)\}\}/g)].reduce((acc, cur) => acc.replace(cur[0], this.data[cur[1].trim()]), tem))
       });
     }
   }
