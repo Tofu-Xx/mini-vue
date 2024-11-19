b5d8c9222fd85591c849ca735219c5355eb22ddf
Tofu-Xx <1714843907@qq.com>
2024-11-20 01:03:15 +0800

    refactor: iife entry

--- vue.js
+++ vue.js
@@ -8,15 +8,14 @@ class Vue {
       set: (...args) => [Reflect.set(...args), this.#depsMap[args[1]].forEach(f => f())][0],
     });
     this.methods = opt.methods;
-    this.#entry(this.el, document.createTreeWalker(this.el, NodeFilter.SHOW_ALL), fn => (this.#activeEffect = fn, fn()));
-  }
-  #entry(node, walker, effect) {
-    if (node instanceof Element) for (let attr of node.attributes) {
-      attr.name[0] === '@' && node.addEventListener(attr.name.slice(1), this.methods[attr.value].bind(this.data));
-      attr.name[0] === ':' && effect(() => node.setAttribute(node.__v__ = attr.name.slice(1), node[node.__v__] = this.data[attr.value]));
-    }
-    if (node instanceof Text && (node.__v_tem__ = node.textContent) && (node.__v__ = Array(...node.__v_tem__.matchAll(/\{\{(.*?)\}\}/g))).length)
-      effect(() => node.textContent = node.__v__.reduce((acc, cur) => acc.replace(cur[0], this.data[cur[1].trim()]), node.__v_tem__));
-    walker.nextNode() && this.#entry(walker.currentNode, walker, effect);
+    (function entry(node, walker, effect) {
+      if (node instanceof Element) for (let attr of node.attributes) {
+        attr.name[0] === '@' && node.addEventListener(attr.name.slice(1), this.methods[attr.value].bind(this.data));
+        attr.name[0] === ':' && effect(() => node.setAttribute(node.__v__ = attr.name.slice(1), node[node.__v__] = this.data[attr.value]));
+      }
+      if (node instanceof Text && (node.__v_tem__ = node.textContent) && (node.__v__ = Array(...node.__v_tem__.matchAll(/\{\{(.*?)\}\}/g))).length)
+        effect(() => node.textContent = node.__v__.reduce((acc, cur) => acc.replace(cur[0], this.data[cur[1].trim()]), node.__v_tem__));
+      walker.nextNode() && entry.bind(this)(walker.currentNode, walker, effect);
+    }).bind(this)(this.el, document.createTreeWalker(this.el, NodeFilter.SHOW_ALL), fn => (this.#activeEffect = fn, fn()));
   }
 }
