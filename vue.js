fe5742f84ab7eae1e1564c08ac15a1a92393ea37
Tofu-Xx <1714843907@qq.com>
2024-11-19 21:23:47 +0800

    refactor: only Vue class

--- vue.js
+++ vue.js
@@ -1,24 +1,23 @@
-let activeEffect = null;
-const effect = (fn) => (function _effect() {
-    activeEffect = _effect, fn();
-})();
-const depsMap = Object.create(null);
-const track = (key) => (depsMap[key] ||= new Set()).add(activeEffect);
-const trigger = (key) => depsMap[key].forEach((effect) => effect());
-const reactive = (target) => new Proxy(target, [['get', track], ['set', trigger]].reduce((acc, [type, toDeps]) => (acc[type] = (...args) => {
-    const res = Reflect[type](...args);
-    return toDeps(args[1]), res;
-}, acc), {}));
-window.Vue = class Vue {
+class Vue {
+    static #depsMap = Object.create(null);
+    static #activeEffect = null;
     constructor (opt) {
         this.el = opt.el instanceof Element ? opt.el : document.querySelector(opt.el) ?? document.createDocumentFragment();
-        this.data = reactive(opt.data);
+        this.data = new Proxy(opt.data, {
+            get: (...args) => [Reflect.get(...args),(Vue.#depsMap[args[1]] ||= new Set()).add(Vue.#activeEffect)][0],
+            set: (...args) => [Reflect.set(...args),Vue.#depsMap[args[1]].forEach((effect) => effect())][0]
+        })
         this.methods = opt.methods;
         this.#entry(this.el, document.createTreeWalker(this.el, NodeFilter.SHOW_ELEMENT));
     }
     #entry(node, walker) {
-        [...node.attributes].forEach((attr) => attr.name[0] === "@" ? node.addEventListener(attr.name.slice(1), this.methods[attr.value].bind(this.data)) : attr.name[0] === ":" && effect(() => node.setAttribute(node.slot = attr.name.slice(1), node[node.slot] = this.data[attr.value])));
-        node.childNodes.forEach((child) => (node.slot = child.nodeType === Node.TEXT_NODE && child.textContent) && effect(() => child.textContent = [...node.slot.matchAll(/\{\{(.*?)\}\}/g)].reduce((acc, cur) => acc.replace(cur[0], this.data[cur[1].trim()]), node.slot)));
+        [...node.attributes].forEach((attr) => attr.name[0] === "@" ? node.addEventListener(attr.name.slice(1), this.methods[attr.value].bind(this.data)) : attr.name[0] === ":" && this.#effect(() => node.setAttribute(node.slot = attr.name.slice(1), node[node.slot] = this.data[attr.value])));
+        node.childNodes.forEach((child) => (node.slot = child.nodeType === Node.TEXT_NODE && child.textContent) && this.#effect(() => child.textContent = [...node.slot.matchAll(/\{\{(.*?)\}\}/g)].reduce((acc, cur) => acc.replace(cur[0], this.data[cur[1].trim()]), node.slot)));
         walker.nextNode() && this.#entry(walker.currentNode, walker);
+    }
+    #effect(fn) {
+        return (function _effect() {
+            Vue.#activeEffect = _effect, fn();
+        })();
     }
 };
