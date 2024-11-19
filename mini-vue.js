2d0b83039435d100a7b7c095469e1a0e64628de4
Tofu-Xx <1714843907@qq.com>
2024-11-20 07:58:44 +0800

    fix: no watch case

--- mini-vue.js
+++ mini-vue.js
@@ -1,10 +1,10 @@
 function Vue(opt) {
   const _ = { deps: Object.create(null), active: () => void 0 };
-  const $el = opt.el instanceof Element ? opt.el : document.querySelector(opt.el) ?? document.createDocumentFragment();
-  const that = Object.assign(new Proxy(opt.data instanceof Function ? opt.data() : opt.data, {
+  const $el = opt?.el instanceof Element ? opt?.el : document.querySelector(opt?.el) ?? document.createDocumentFragment();
+  const that = Object.assign(new Proxy(opt?.data instanceof Function ? opt?.data() : opt?.data, {
     get: (...args) => [Reflect.get(...args), (_.deps[args[1]] ||= new Set()).add(_.active)][0],
     set: (...args) => [Reflect.set(...args), _.deps[args[1]]?.forEach(f => f())][0],
-  }), opt.methods, { $el, $refs: Object.create(null) });
+  }), opt?.methods, { $el, $refs: Object.create(null) });
   (function walk(node, walker, effect) {
     if (node instanceof Element) for (let attr of node.attributes) {
       attr.name[0] === '@' && node.addEventListener(attr.name.slice(1), that[attr.value].bind(that));
@@ -15,7 +15,7 @@ function Vue(opt) {
       effect(() => node.data = node.__v__.reduce((acc, cur) => acc.replace(cur[0], that[cur[1]?.trim()]), node.__v_tem__));
     walker.nextNode() && walk.bind(that)(walker.currentNode, walker, effect);
   })($el, document.createTreeWalker($el, NodeFilter.SHOW_ALL), fn => (_.active = fn, fn()));
-  for (const [k, f] of Object.entries(opt.watch))
+  for (const [k, f] of Object.entries(opt?.watch ?? {}))
     f.oldVal = that[k], _.deps[k].add(() => Promise.resolve().then(() => (f.val = that[k], f.bind(that)(f.val, f.oldVal), f.oldVal = f.val)));
-  opt.mounted?.bind(that)();
+  opt?.mounted?.bind(that)();
 };
