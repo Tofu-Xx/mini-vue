90b9444ceb12596e9fd7dff29a78fea657e86cd1
Tofu-Xx <1714843907@qq.com>
2024-11-20 04:52:58 +0800

    chore: Text data

--- vue.js
+++ vue.js
@@ -1,21 +1,20 @@
 function Vue(opt) {
-  const $ = { deps: Object.create(null) };
+  const _ = { deps: Object.create(null) };
   const $el = opt.el instanceof Element ? opt.el : document.querySelector(opt.el) ?? document.createDocumentFragment();
-  const $data = new Proxy(opt.data instanceof Function ? opt.data() : opt.data, {
-    get: (...args) => [Reflect.get(...args), ($.deps[args[1]] ||= new Set()).add($.active)][0],
-    set: (...args) => [Reflect.set(...args), $.deps[args[1]]?.forEach(f => f())][0],
-  });
-  Object.assign(this, opt.methods, { $el, $data, $refs: {} });
+  const that = Object.assign(new Proxy(opt.data instanceof Function ? opt.data() : opt.data, {
+    get: (...args) => [Reflect.get(...args), (_.deps[args[1]] ||= new Set()).add(_.active)][0],
+    set: (...args) => [Reflect.set(...args), _.deps[args[1]]?.forEach(f => f())][0],
+  }), opt.methods, { $el, $refs: {} });
   (function entry(node, walker, effect) {
     if (node instanceof Element) for (let attr of node.attributes) {
-      attr.name[0] === '@' && node.addEventListener(attr.name.slice(1), this[attr.value]?.bind($data));
-      attr.name[0] === ':' && effect(() => node.setAttribute(attr.__v__ = attr.name.slice(1), node[attr.__v__] = $data[attr.value]));
-      attr.name === 'ref' && (this.$refs[attr.value] = node);
+      attr.name[0] === '@' && node.addEventListener(attr.name.slice(1), that[attr.value]?.bind(that));
+      attr.name[0] === ':' && effect(() => node.setAttribute(attr.__v__ = attr.name.slice(1), node[attr.__v__] = that[attr.value]));
+      attr.name === 'ref' && (that.$refs[attr.value] = node);
     }
-    if (node instanceof Text && (node.__v_tem__ = node.textContent) && (node.__v__ = Array(...node.__v_tem__.matchAll(/\{\{(.*?)\}\}/g))).length)
-      effect(() => node.textContent = node.__v__.reduce((acc, cur) => acc.replace(cur[0], $data[cur[1]?.trim()]), node.__v_tem__));
-    walker.nextNode() && entry?.bind(this)(walker.currentNode, walker, effect);
-  })?.bind(this)($el, document.createTreeWalker($el, NodeFilter.SHOW_ALL), fn => ($.active = fn, fn()));
-  Object.keys(opt.watch).forEach(k => $.deps[k]?.add(() => setTimeout(() => opt.watch[k]?.bind($data)($data[k]), 0)));
-  opt.mounted?.bind(this)();
+    if (node instanceof Text && (node.__v_tem__ = node.data) && (node.__v__ = Array(...node.__v_tem__.matchAll(/\{\{(.*?)\}\}/g)))[0])
+      effect(() => node.data = node.__v__.reduce((acc, cur) => acc.replace(cur[0], that[cur[1]?.trim()]), node.__v_tem__));
+    walker.nextNode() && entry?.bind(that)(walker.currentNode, walker, effect);
+  })($el, document.createTreeWalker($el, NodeFilter.SHOW_ALL), fn => (_.active = fn, fn()));
+  Object.keys(opt.watch).forEach(k => _.deps[k]?.add(() => setTimeout(() => opt.watch[k]?.bind(that)(that[k]), 0)));
+  opt.mounted?.bind(that)();
 };
