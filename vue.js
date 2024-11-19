5662716e49e37d779706d213db86b04fe052c769
Tofu-Xx <1714843907@qq.com>
2024-11-20 00:46:31 +0800

    refactor: no effect

--- vue.js
+++ vue.js
@@ -1,25 +1,22 @@
 class Vue {
-  static #activeEffect = () => { };
+  #activeEffect = () => { };
   #depsMap = Object.create(null);
   constructor (opt) {
     this.el = opt.el instanceof Element ? opt.el : document.querySelector(opt.el) ?? document.createDocumentFragment();
     this.data = new Proxy(opt.data instanceof Function ? opt.data() : opt.data, {
-      get: (...args) => [Reflect.get(...args), (this.#depsMap[args[1]] ||= new Set()).add(Vue.#activeEffect)][0],
+      get: (...args) => [Reflect.get(...args), (this.#depsMap[args[1]] ||= new Set()).add(this.#activeEffect)][0],
       set: (...args) => [Reflect.set(...args), this.#depsMap[args[1]].forEach(f => f())][0],
     });
     this.methods = opt.methods;
-    this.#entry(this.el, document.createTreeWalker(this.el, NodeFilter.SHOW_ALL));
+    this.#entry(this.el, document.createTreeWalker(this.el, NodeFilter.SHOW_ALL), fn => (this.#activeEffect = fn, fn()));
   }
-  #entry(node, walker) {
+  #entry(node, walker, effect) {
     if (node instanceof Element) for (let attr of node.attributes) {
       attr.name[0] === '@' && node.addEventListener(attr.name.slice(1), this.methods[attr.value].bind(this.data));
-      attr.name[0] === ':' && this.#effect(() => node.setAttribute(node.__v__ = attr.name.slice(1), node[node.__v__] = this.data[attr.value]));
+      attr.name[0] === ':' && effect(() => node.setAttribute(node.__v__ = attr.name.slice(1), node[node.__v__] = this.data[attr.value]));
     }
     if (node instanceof Text && (node.__v_tem__ = node.textContent) && (node.__v__ = Array(...node.__v_tem__.matchAll(/\{\{(.*?)\}\}/g))).length)
-      this.#effect(() => node.textContent = node.__v__.reduce((acc, cur) => acc.replace(cur[0], this.data[cur[1].trim()]), node.__v_tem__));
-    walker.nextNode() && this.#entry(walker.currentNode, walker);
+      effect(() => node.textContent = node.__v__.reduce((acc, cur) => acc.replace(cur[0], this.data[cur[1].trim()]), node.__v_tem__));
+    walker.nextNode() && this.#entry(walker.currentNode, walker, effect);
   }
-  #effect = fn => (function run() {
-    Vue.#activeEffect = run, fn();
-  })();
 }
