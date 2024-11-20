f45a3cd963ead4da46101b1ef650e5dadbda1e3b
Tofu-Xx <1714843907@qq.com>
2024-11-20 12:23:20 +0800

    chore: no data

--- mini-vue.js
+++ mini-vue.js
@@ -1,9 +1,11 @@
 function Vue(opt) {
-  const _ = { deps: {} };
-  const $el = typeof opt.el == 'string' ? document.querySelector(opt.el) : opt.el ?? document;
-  const that = Object.assign(new Proxy(opt.data instanceof Function ? opt.data() : opt.data ?? {}, {
-    get: (...args) => [Reflect.get(...args), (_.deps[args[1]] ??= new Set()).add(_.active)][0],
-    set: (...args) => [Reflect.set(...args), _.deps[args[1]]?.forEach(f => f())][0],
+  let active;
+  const deps = {};
+  const $el = opt.el?.at ? document.querySelector(opt.el) : opt.el ?? document
+  if (!$el) return;
+  const that = Object.assign(new Proxy(typeof opt.data == 'object' ? opt.data : opt.data?.() ?? {}, {
+    get: (...args) => [Reflect.get(...args), (deps[args[1]] ??= new Set()).add(active)][0],
+    set: (...args) => [Reflect.set(...args), deps[args[1]]?.forEach(f => f())][0],
   }), opt.methods, { $el, $refs: {} });
   (function walk(node, walker, effect) {
     if (node.nodeType == 1) for (let attr of node.attributes) {
@@ -14,10 +16,20 @@ function Vue(opt) {
         effect(() => node.setAttribute(name, node[name] = that[attr.value]));
       }
     }
-    if (node.nodeType == 3 && (node.__v_tem__ = node.data) && (node.__v__ = Array(...node.__v_tem__.matchAll(/\{\{(.*?)\}\}/g)))[0])
-      effect(() => node.data = node.__v__.reduce((acc, cur) => acc.replace(cur[0], that[cur[1].trim()]), node.__v_tem__));
+    if (node.nodeType == 3) {
+      const tem = node.data;
+      const matches = Array(...tem.matchAll(/\{\{(.*?)\}\}/g));
+      matches[0] && effect(() => node.data = matches.reduce((acc, cur) => acc.replace(cur[0], that[cur[1].trim()]), tem));
+    }
     walker.nextNode() ? walk.bind(that)(walker.currentNode, walker, effect) : opt.mounted?.bind(that)();
-  })($el, document.createTreeWalker($el, 4294967295), fn => (_.active = fn, fn()));
-  for (const [k, f] of Object.entries(opt.watch ?? {}))
-    f.oldVal = that[k], _.deps[k].add(() => Promise.resolve().then(() => (f.val = that[k], f.bind(that)(f.val, f.oldVal), f.oldVal = f.val)));
+  })($el, document.createTreeWalker($el, 7), fn => (active = fn, fn()));
+  /*  */
+  for (const [k, f] of Object.entries(opt.watch ?? {})) {
+    let oldVal = that[k];
+    deps[k].add(() => Promise.resolve().then(() => {
+      let val = that[k];
+      f.bind(that)(val, oldVal);
+      oldVal = val;
+    }));
+  }
 };
