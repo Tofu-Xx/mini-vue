f5b5767cfb640acb2c359190e406222c3beedfc4
Tofu-Xx <1714843907@qq.com>
2024-11-20 13:24:58 +0800

    chore: 927

--- mini-vue.js
+++ mini-vue.js
@@ -6,10 +6,12 @@ function Vue(opt) {
     get: (...args) => [Reflect.get(...args), (deps[args[1]] ??= new Set()).add(active)][0],
     set: (...args) => [Reflect.set(...args), deps[args[1]]?.forEach(f => f())][0],
   }), opt.methods, { $el, $refs: {} });
-  const walk = (walker, node, effect) => {
+  const walk = (walker, effect) => {
+    const node = walker.currentNode;
     if (node.nodeType == 1) for (let attr of node.attributes) {
       attr.name == 'ref' && (that.$refs[attr.value] = node);
-      attr.name[0] == '@' && node.addEventListener(attr.name.slice(1), that[attr.value].bind(that));
+      attr.name[0] == '@' && (node[attr.name.replace('@', 'on')] = that[attr.value].bind(that));
+      // attr.name[0] == '@' && node.addEventListener(attr.name.slice(1), that[attr.value].bind(that));
       if (attr.name[0] == ':') {
         const name = attr.name.slice(1);
         effect(() => node.setAttribute(name, node[name] = that[attr.value]));
@@ -20,13 +22,13 @@ function Vue(opt) {
       const matches = Array(...tem.matchAll(/\{\{(.*?)\}\}/g));
       matches[0] && effect(() => node.data = matches.reduce((acc, cur) => acc.replace(cur[0], that[cur[1].trim()]), tem));
     }
-    walker.nextNode() ? walk(walker, walker.currentNode, effect) : opt.mounted?.bind(that)();
+    walker.nextNode() ? walk(walker, effect) : opt.mounted?.bind(that)();
   };
-  walk(document.createTreeWalker($el, 7), $el, fn => (active = fn, fn()));
+  walk(document.createTreeWalker($el, 7), fn => (active = fn, fn()));
   for (const [k, f] of Object.entries(opt.watch ?? {})) {
     let oldVal = that[k];
     deps[k].add(() => Promise.resolve().then(() => {
-      let val = that[k];
+      const val = that[k];
       f.bind(that)(val, oldVal);
       oldVal = val;
     }));
