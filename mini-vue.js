600e367f6dde372ef66d26377f2bcfb775bb05fb
Tofu-Xx <1714843907@qq.com>
2024-11-20 10:45:03 +0800

    chore: 4294967295

--- mini-vue.js
+++ mini-vue.js
@@ -7,14 +7,17 @@ function Vue(opt) {
   }), opt.methods, { $el, $refs: {} });
   (function walk(node, walker, effect) {
     if (node.nodeType == 1) for (let attr of node.attributes) {
-      attr.name[0] == '@' && node.addEventListener(attr.name.slice(1), that[attr.value].bind(that));
-      attr.name[0] == ':' && effect(() => node.setAttribute(attr.__v__ = attr.name.slice(1), node[attr.__v__] = that[attr.value]));
       attr.name == 'ref' && (that.$refs[attr.value] = node);
+      attr.name[0] == '@' && node.addEventListener(attr.name.slice(1), that[attr.value].bind(that));
+      if (attr.name[0] == ':') {
+        const name = attr.name.slice(1);
+        effect(() => node.setAttribute(name, node[name] = that[attr.value]));
+      }
     }
     if (node.nodeType == 3 && (node.__v_tem__ = node.data) && (node.__v__ = Array(...node.__v_tem__.matchAll(/\{\{(.*?)\}\}/g)))[0])
       effect(() => node.data = node.__v__.reduce((acc, cur) => acc.replace(cur[0], that[cur[1].trim()]), node.__v_tem__));
     walker.nextNode() ? walk.bind(that)(walker.currentNode, walker, effect) : opt.mounted?.bind(that)();
-  })($el, document.createTreeWalker($el, NodeFilter.SHOW_ALL), fn => (_.active = fn, fn()));
+  })($el, document.createTreeWalker($el, 4294967295), fn => (_.active = fn, fn()));
   for (const [k, f] of Object.entries(opt.watch ?? {}))
     f.oldVal = that[k], _.deps[k].add(() => Promise.resolve().then(() => (f.val = that[k], f.bind(that)(f.val, f.oldVal), f.oldVal = f.val)));
 };
