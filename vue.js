826cd81f0c45a6a1569da02c87963a71c496898b
Tofu-Xx <1714843907@qq.com>
2024-11-21 00:55:00 +0800

    feat: attr Expression

--- vue.js
+++ vue.js
@@ -4,27 +4,27 @@ function Vue(opt) {
   const $el = opt.el?.at ? document.querySelector(opt.el) : opt.el ?? document;
   const that = Object.assign(new Proxy(typeof opt.data == 'object' ? opt.data : opt.data?.() ?? {}, {
     get: (...args) => [Reflect.get(...args), (deps[args[1]] ??= new Set()).add(active)][0],
-    set: (...args) => [Reflect.set(...args), deps[args[1]]?.forEach(f => f())][0],
+    set: (...args) => [Reflect.set(...args), deps[args[1]]?.forEach(f => (console.log(f), f?.()))][0],
   }), opt.methods, { $el, $refs: {} });
   const thatKeyRex = new RegExp(Object.keys(that).join('\\b|').replaceAll('$', '\\$'), 'g');
   function parseExpression(raw) {
     console.log(thatKeyRex);
-    const expr = raw.replace(thatKeyRex, k => 'that.'+k);
+    const expr = raw.replace(thatKeyRex, k => 'that.' + k);
     return eval(expr);
   }
   const walk = (walker, effect) => {
     const node = walker.currentNode;
     const { nodeType, data: tem } = node;
-    if (nodeType == 1) for (const { name, value } of node.attributes) {
-      name == 'ref' && (that.$refs[value] = node);
-      name[0] == '@' && (node[name.replace('@', 'on')] = that[value].bind(that));
+    if (nodeType == 1) for (const { name, value: raw } of node.attributes) {
+      name == 'ref' && (that.$refs[raw] = node);
+      name[0] == '@' && (node[name.replace('@', 'on')] = that[raw].bind(that));
       if (name[0] == ':') {
         const bindName = name.slice(1);
-        effect(() => node.setAttribute(bindName, node[bindName] = that[value]));
+        effect(() => node.setAttribute(bindName, node[bindName] = parseExpression(raw)));
       }
     }
     if (nodeType == 3) {
-      node.data = tem.replace(/\{\{(.*?)\}\}/g, (_, raw) => parseExpression(raw));
+      effect(() => node.data = tem.replace(/\{\{(.*?)\}\}/g, (_, raw) => parseExpression(raw)));
     }
     walker.nextNode() ? walk(walker, effect) : opt.mounted?.call(that);
   };
