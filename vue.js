20d4c21fb08a06d8b1b5dead210cb10fb4da4a3e
Tofu-Xx <1714843907@qq.com>
2024-11-22 03:31:32 +0800

    fix: inline event has ;

--- vue.js
+++ vue.js
@@ -7,7 +7,7 @@ function Vue(opt = {}) {
     set: (...args) => [Reflect.set(...args), _deps[args[1]]?.forEach(f => f?.())][0],
   }), opt.methods, { $el, $refs: {} });
   const thatKeyRex = RegExp(Object.keys(This).join('\\b|').replaceAll('$', '\\$'), 'g');
-  const parseExpression = (raw) => Function('$event', 'return ' + raw.replace(thatKeyRex, k => 'this.' + k));
+  const parseExpression = (raw, preCode = 'return ') => Function('$event', preCode + raw.replace(thatKeyRex, k => 'this.' + k));
   const effect = (fn) => (_active = fn, fn());
   const walk = (walker) => {
     const node = walker.currentNode;
@@ -15,7 +15,7 @@ function Vue(opt = {}) {
     if (nodeType == Node.ELEMENT_NODE) for (const { name, value: raw } of node.attributes) {
       const bindName = name.slice(1);
       if (name[0] == '@')
-        node['on' + bindName] = (/[^\s\w$]/.test(raw) ? parseExpression(raw) : This[raw.trim()])?.bind(This);
+        node['on' + bindName] = (/[^\s\w$]/.test(raw) ? parseExpression(raw, '') : This[raw.trim()])?.bind(This);
       if (name[0] == ':')
         effect(() => node.setAttribute(bindName, node[bindName] = parseExpression(raw).call(This)));
       if (name == 'ref')
