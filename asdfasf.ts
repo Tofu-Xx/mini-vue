fe52225919a4f0e476d974163607d34594100bf0
Tofu-Xx <1714843907@qq.com>
2024-11-21 19:01:17 +0800

    chore: rename that

--- asdfasf.ts
+++
@@ -1,7 +0,0 @@
-interface Options  {
-  el: Element|string,
-  data: Function|Object,
-  methods: Record<string, Function>,
-  watch: Record<string, (val: any, oldVal: any) => void>,
-  mounted: Function,
-}
--- vue.js
+++ vue.js
@@ -2,11 +2,11 @@ function Vue(opt) {
   let _active;
   const _deps = {};
   const $el = opt.el?.at ? document.querySelector(opt.el) : opt.el ?? document;
-  const that = Object.assign(new Proxy(typeof opt.data == 'object' ? opt.data : opt.data?.() ?? {}, {
+  const This = Object.assign(new Proxy(typeof opt.data == 'object' ? opt.data : opt.data?.() ?? {}, {
     get: (...args) => [Reflect.get(...args), (_deps[args[1]] ??= new Set()).add(_active)][0],
     set: (...args) => [Reflect.set(...args), _deps[args[1]]?.forEach(f => f?.())][0],
   }), opt.methods, { $el, $refs: {} });
-  const thatKeyRex = new RegExp(Object.keys(that).join('\\b|').replaceAll('$', '\\$'), 'g');
+  const thatKeyRex = new RegExp(Object.keys(This).join('\\b|').replaceAll('$', '\\$'), 'g');
   const toExpression = (raw) => new Function('$event', 'return ' + raw.replace(thatKeyRex, k => 'this.' + k));
   const walk = (walker, effect) => {
     const node = walker.currentNode;
@@ -14,21 +14,21 @@ function Vue(opt) {
     if (nodeType == 1) for (const { name, value: raw } of node.attributes) {
       const bindName = name.slice(1);
       if (name[0] == '@')
-        node['on' + bindName] = (/[^\s\w$]/.test(raw) ? toExpression(raw) : that[raw.trim()])?.bind(that);
+        node['on' + bindName] = (/[^\s\w$]/.test(raw) ? toExpression(raw) : This[raw.trim()])?.bind(This);
       if (name[0] == ':')
-        effect(() => node.setAttribute(bindName, node[bindName] = toExpression(raw).call(that)));
-      name == 'ref' && (that.$refs[raw] = node);
+        effect(() => node.setAttribute(bindName, node[bindName] = toExpression(raw).call(This)));
+      name == 'ref' && (This.$refs[raw] = node);
     }
     if (nodeType == 3)
-      effect(() => node.data = tem.replace(/\{\{(.*?)\}\}/g, (_, raw) => toExpression(raw).call(that)));
-    walker.nextNode() ? walk(walker, effect) : opt.mounted?.call(that);
+      effect(() => node.data = tem.replace(/\{\{(.*?)\}\}/g, (_, raw) => toExpression(raw).call(This)));
+    walker.nextNode() ? walk(walker, effect) : opt.mounted?.call(This);
   };
   walk(document.createTreeWalker($el, 7), fn => (_active = fn, fn()));
   for (const [key, fn] of Object.entries(opt.watch ?? {})) {
-    let oldVal = that[key];
+    let oldVal = This[key];
     _deps[key].add(() => Promise.resolve().then(() => {
-      const val = that[key];
-      fn.call(that, val, oldVal);
+      const val = This[key];
+      fn.call(This, val, oldVal);
       oldVal = val;
     }));
   }
