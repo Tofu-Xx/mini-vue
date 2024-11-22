9df75876d8f84aebbf9113ce66005ee4e6a63fe6
Tofu-Xx <1714843907@qq.com>
2024-11-22 17:02:15 +0800

    chore: vueey.opt

--- vue.undevil.js
+++ vue.undevil.js
@@ -1,27 +1,29 @@
 /* Stop updating at v0.0.3 */
 function Vue(opt) {
-  window.vueey = Object.create(null),
-  vueey.reactivity = Object.create(null),
-  vueey.reactivity._deps = Object.create(null),
-  vueey.reactivity.trace = (key) => (vueey.reactivity._deps[key] ??= new Set()).add(vueey.reactivity._active),
-  vueey.reactivity.tirgger = (key) => vueey.reactivity._deps[key]?.forEach((f) => f?.()),
-  vueey.reactivity.effect = (fn) => (vueey.reactivity._active = fn, fn()),
-  vueey.el = opt.el?.at ? document.querySelector(opt.el) : opt.el ?? document,
-  vueey.This = Object.assign(
-    new Proxy(
-      'object' == typeof opt.data ? opt.data : opt.data?.() ?? Object.create(null),
+  (
+    window.vueey = Object.create(null),
+    vueey.opt = opt,
+    vueey.reactivity = Object.create(null),
+    vueey.reactivity._deps = Object.create(null),
+    vueey.reactivity.trace = (key) => (vueey.reactivity._deps[key] ??= new Set()).add(vueey.reactivity._active),
+    vueey.reactivity.tirgger = (key) => vueey.reactivity._deps[key]?.forEach((f) => f?.()),
+    vueey.reactivity.effect = (fn) => (vueey.reactivity._active = fn, fn()),
+    vueey.el = opt?.el?.at ? document.querySelector(opt?.el) : opt?.el ?? document,
+    vueey.This = Object.assign(
+      new Proxy(
+        'object' == typeof opt?.data ? opt?.data : opt?.data?.() ?? Object.create(null),
+        Object.fromEntries([
+          ["get", (...args) => [Reflect.get(...args), vueey.reactivity.trace(args[1])][0]],
+          ["set", (...args) => [Reflect.set(...args), vueey.reactivity.tirgger(args[1])][0]],
+        ])
+      ),
+      opt?.methods,
       Object.fromEntries([
-        ["get", (...args) => [Reflect.get(...args), vueey.reactivity.trace(args[1])][0]],
-        ["set", (...args) => [Reflect.set(...args), vueey.reactivity.tirgger(args[1])][0]],
+        ["$el", vueey.el,],
+        ["$refs", Object.create(null)]
       ])
     ),
-    opt.methods,
-    Object.fromEntries([
-      ["$el", vueey.el,],
-      ["$refs", Object.create(null)]
-    ])
-  ),
-  vueey.infuse = (energy, raw, preCode = 'return ') => Function(
+    vueey.infuse = (energy, raw, preCode = 'return ') => Function(
       '$event',
       preCode + raw.trim().replace(
         RegExp(
@@ -30,8 +32,8 @@ function Vue(opt) {
         ),
         (k) => 'this.' + k
       )
-  ).bind(energy),
-  vueey.forof = (iterable, callback) => (
+    ).bind(energy),
+    vueey.forof = (iterable, callback) => (
       vueey.forof.iterable = Object.create(null),
       vueey.forof.iterable[iterable] = iterable[Symbol.iterator](),
       vueey.forof.next = () => (
@@ -43,58 +45,59 @@ function Vue(opt) {
         )
       ),
       vueey.forof.next()
-  ),
-  vueey.compiler = (node, walker = document.createTreeWalker(node, NodeFilter.SHOW_ALL)) => (
-    node.nodeType == Node.ELEMENT_NODE && vueey.forof(node.attributes, (attr) => (
-      'ref' == attr.name && (
-        vueey.This.$refs[attr.value] = node
-      ),
-      '@' == attr.name[0] && (
-        node[attr.name.replace('@', 'on')] = /[^\s\w$]/.test(attr.value)
-          ? vueey.infuse(vueey.This, attr.value, '')
-          : vueey.This[attr.value.trim()].bind(vueey.This)
-      ),
-      ':' == attr.name[0] && (
-        vueey.compiler._binds.push(attr.name.slice(1)),
-        vueey.reactivity.effect(
-          () => node.setAttribute(
-            vueey.compiler._binds.at(-1),
-            node[vueey.compiler._binds.at(-1)] = vueey.infuse(vueey.This, attr.value)()
+    ),
+    vueey.compiler = (node = vueey.el, walker = document.createTreeWalker(node, NodeFilter.SHOW_ALL)) => (
+      node.nodeType == Node.ELEMENT_NODE && vueey.forof(node.attributes, (attr) => (
+        'ref' == attr.name && (
+          vueey.This.$refs[attr.value] = node
+        ),
+        '@' == attr.name[0] && (
+          node[attr.name.replace('@', 'on')] = /[^\s\w$]/.test(attr.value)
+            ? vueey.infuse(vueey.This, attr.value, '')
+            : vueey.This[attr.value.trim()].bind(vueey.This)
+        ),
+        ':' == attr.name[0] && (
+          vueey.compiler._binds.push(attr.name.slice(1)),
+          vueey.reactivity.effect(
+            () => node.setAttribute(
+              vueey.compiler._binds.at(-1),
+              node[vueey.compiler._binds.at(-1)] = vueey.infuse(vueey.This, attr.value)()
+            )
           )
         )
-      )
-    )),
-    node.nodeType == Node.TEXT_NODE && (
-      node.__raw_tem__ = node.data,
-      vueey.reactivity.effect(() => (
-        node.data = node.__raw_tem__.replace(
-          /\{\{(.*?)\}\}/g,
-          (_, raw) => vueey.infuse(vueey.This, raw)()
-        ))
-      )
+      )),
+      node.nodeType == Node.TEXT_NODE && (
+        node.__raw_tem__ = node.data,
+        vueey.reactivity.effect(() => (
+          node.data = node.__raw_tem__.replace(
+            /\{\{(.*?)\}\}/g,
+            (_, raw) => vueey.infuse(vueey.This, raw)()
+          ))
+        )
+      ),
+      walker.nextNode()
+        ? vueey.compiler(walker.currentNode, walker)
+        : opt?.mounted?.call(vueey.This)
     ),
-    walker.nextNode()
-      ? vueey.compiler(walker.currentNode, walker)
-      : opt.mounted?.call(vueey.This)
-  ),
-  vueey.compiler._binds = [],
-  vueey.compiler(vueey.el),
-  vueey.forof(
-    Object.entries(opt.watch ?? Object.create(null)),
-    ([key, fn]) => (
-      vueey.watchedOldVal = Object.create(null),
-      vueey.watchedOldVal[key] = vueey.This[key],
-      vueey.reactivity._deps[key].add(
-        () => Promise.resolve().then(
-          () => (
-            vueey.watchedVal = Object.create(null),
-            vueey.watchedVal[key] = vueey.This[key],
-            fn.call(
-              vueey.This,
-              vueey.watchedVal[key],
-              vueey.watchedOldVal[key]
-            ),
-            vueey.watchedOldVal[key] = vueey.watchedVal[key]
+    vueey.compiler._binds = [],
+    vueey.opt && vueey.compiler(vueey.el),
+    vueey.forof(
+      Object.entries(opt?.watch ?? Object.create(null)),
+      ([key, fn]) => (
+        vueey.watchedOldVal = Object.create(null),
+        vueey.watchedOldVal[key] = vueey.This[key],
+        vueey.reactivity._deps[key].add(
+          () => Promise.resolve().then(
+            () => (
+              vueey.watchedVal = Object.create(null),
+              vueey.watchedVal[key] = vueey.This[key],
+              fn.call(
+                vueey.This,
+                vueey.watchedVal[key],
+                vueey.watchedOldVal[key]
+              ),
+              vueey.watchedOldVal[key] = vueey.watchedVal[key]
+            )
           )
         )
       )
