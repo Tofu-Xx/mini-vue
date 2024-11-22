174e8760700c2535d0b49a901e18d930b5014f9f
Tofu-Xx <1714843907@qq.com>
2024-11-22 15:32:41 +0800

    chore!: shit ok

--- vue.shit.unmin.js
+++ vue.shit.unmin.js
@@ -1,17 +1,18 @@
 /* 0.0.3 */
 function Vue(opt) {
   window.vueey = Object.create(null),
-  vueey._deps = Object.create(null),
+  vueey.reactivity = Object.create(null),
+  vueey.reactivity._deps = Object.create(null),
+  vueey.reactivity.trace = (key) => (vueey.reactivity._deps[key] ??= new Set()).add(vueey.reactivity._active),
+  vueey.reactivity.tirgger = (key) => vueey.reactivity._deps[key]?.forEach((f) => f?.()),
+  vueey.reactivity.effect = (fn) => (vueey.reactivity._active = fn, fn()),
   vueey.el = opt.el?.at ? document.querySelector(opt.el) : opt.el ?? document,
-  vueey.trace = (key) => (vueey._deps[key] ??= new Set()).add(vueey._active),
-  vueey.tirgger = (key) => vueey._deps[key]?.forEach((f) => f?.()),
-  vueey.effect = (fn) => (vueey._active = fn, fn()),
   vueey.This = Object.assign(
     new Proxy(
       'object' == typeof opt.data ? opt.data : opt.data?.() ?? Object.create(null),
       Object.fromEntries([
-        ["get", (...args) => [Reflect.get(...args), vueey.trace(args[1])][0]],
-        ["set", (...args) => [Reflect.set(...args), vueey.tirgger(args[1])][0]],
+        ["get", (...args) => [Reflect.get(...args), vueey.reactivity.trace(args[1])][0]],
+        ["set", (...args) => [Reflect.set(...args), vueey.reactivity.tirgger(args[1])][0]],
       ])
     ),
     opt.methods,
@@ -19,55 +20,53 @@ function Vue(opt) {
       ["$el", vueey.el,],
       ["$refs", Object.create(null)]
     ])
-  ), 
-  vueey.infuse = (energy , raw, preCode = 'return ') => Function(
-    '$event',
-    preCode + raw.trim().replace(
-      RegExp(
-        Object.keys(energy).join('\\b|').replaceAll('$', '\\$'),
-        'g'
-      ),
-      (k) => 'this.' + k
-    )
+  ),
+  vueey.infuse = (energy, raw, preCode = 'return ') => Function(
+      '$event',
+      preCode + raw.trim().replace(
+        RegExp(
+          Object.keys(energy).join('\\b|').replaceAll('$', '\\$'),
+          'g'
+        ),
+        (k) => 'this.' + k
+      )
   ).bind(energy),
   vueey.forof = (iterable, callback) => (
-    vueey.forof.iterable = Object.create(null),
-    vueey.forof.iterable[iterable] = iterable[Symbol.iterator](),
-    vueey.forof.next = () => (
-      vueey.forof.result = Object.create(null),
-      vueey.forof.result[iterable] = vueey.forof.iterable[iterable].next(),
-      vueey.forof.result[iterable].done || (
-        callback(vueey.forof.result[iterable].value),
-        vueey.forof.next()
-      )
-    ),
-    vueey.forof.next()
+      vueey.forof.iterable = Object.create(null),
+      vueey.forof.iterable[iterable] = iterable[Symbol.iterator](),
+      vueey.forof.next = () => (
+        vueey.forof.result = Object.create(null),
+        vueey.forof.result[iterable] = vueey.forof.iterable[iterable].next(),
+        vueey.forof.result[iterable].done || (
+          callback(vueey.forof.result[iterable].value),
+          vueey.forof.next()
+        )
+      ),
+      vueey.forof.next()
   ),
-  vueey._binds = [],
   vueey.compiler = (node, walker = document.createTreeWalker(node, NodeFilter.SHOW_ALL)) => (
     node.nodeType == Node.ELEMENT_NODE && vueey.forof(node.attributes, (attr) => (
       'ref' == attr.name && (
         vueey.This.$refs[attr.value] = node
       ),
       '@' == attr.name[0] && (
-        node[attr.name.replace('@', 'on')] = 
-          /[^\s\w$]/.test(attr.value)
-            ? vueey.infuse(vueey.This, attr.value, '')
-            : vueey.This[attr.value.trim()].bind(vueey.This)
+        node[attr.name.replace('@', 'on')] = /[^\s\w$]/.test(attr.value)
+          ? vueey.infuse(vueey.This, attr.value, '')
+          : vueey.This[attr.value.trim()].bind(vueey.This)
       ),
       ':' == attr.name[0] && (
-        vueey._binds.push(attr.name.slice(1)),  
-        vueey.effect(
+        vueey.compiler._binds.push(attr.name.slice(1)),
+        vueey.reactivity.effect(
           () => node.setAttribute(
-            vueey._binds.at(-1),
-            node[vueey._binds.at(-1)] = vueey.infuse(vueey.This, attr.value)()
+            vueey.compiler._binds.at(-1),
+            node[vueey.compiler._binds.at(-1)] = vueey.infuse(vueey.This, attr.value)()
           )
         )
       )
     )),
     node.nodeType == Node.TEXT_NODE && (
       node.__raw_tem__ = node.data,
-      vueey.effect(() => (
+      vueey.reactivity.effect(() => (
         node.data = node.__raw_tem__.replace(
           /\{\{(.*?)\}\}/g,
           (_, raw) => vueey.infuse(vueey.This, raw)()
@@ -78,13 +77,14 @@ function Vue(opt) {
       ? vueey.compiler(walker.currentNode, walker)
       : opt.mounted?.call(vueey.This)
   ),
+  vueey.compiler._binds = [],
   vueey.compiler(vueey.el),
   vueey.forof(
     Object.entries(opt.watch ?? Object.create(null)),
     ([key, fn]) => (
       vueey.watchedOldVal = Object.create(null),
       vueey.watchedOldVal[key] = vueey.This[key],
-      vueey._deps[key].add(
+      vueey.reactivity._deps[key].add(
         () => Promise.resolve().then(
           () => (
             vueey.watchedVal = Object.create(null),
